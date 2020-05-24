a32fb52c2f3f096fdeacadaadc91b90dcd08470d
Charlie Oxendine <ceox921@gmail.com>
2020-05-24 09:47:55 -0400

    Initial Commit

---
+++ learning_firebase_onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learning_firebase_onboarding
+//
+//  Created by Charles Oxendine on 5/24/20.
+//  Copyright © 2020 Charles Oxendine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
